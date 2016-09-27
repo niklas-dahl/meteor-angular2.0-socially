e0ab6d319a814bc5a8f0d5227ca9ca8c4039f22b
dotansimha <dotansimha@gmail.com>
2016-09-27 14:01:42 +0300

    Step 21.6: Create stores for Images and Thumbs

--- both/collections/images.collection.ts
+++ both/collections/images.collection.ts
@@ -1,9 +1,26 @@
 import { MongoObservable } from 'meteor-rxjs';
 import { Meteor } from 'meteor/meteor';
 import {Thumb, Image} from "../models/image.model";
+import { UploadFS } from 'meteor/jalik:ufs';
 
 export const Images = new MongoObservable.Collection<Image>('images');
 export const Thumbs = new MongoObservable.Collection<Thumb>('thumbs');
+
+export const ThumbsStore = new UploadFS.store.GridFS({
+  collection: Thumbs.collection,
+  name: 'thumbs'
+});
+
+export const ImagesStore = new UploadFS.store.GridFS({
+  collection: Images.collection,
+  name: 'images',
+  filter: new UploadFS.Filter({
+    contentTypes: ['image/*']
+  }),
+  copyTo: [
+    ThumbsStore
+  ]
+});
 
 function loggedIn(userId) {
   return !!userId;
