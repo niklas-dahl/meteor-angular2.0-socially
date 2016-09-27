186444da7ff72368bf3d9bb7696c733bef017ef8
dotansimha <dotansimha@gmail.com>
2016-09-27 14:02:18 +0300

    Step 21.7: Resize images

--- both/collections/images.collection.ts
+++ both/collections/images.collection.ts
@@ -8,7 +8,19 @@ export const Thumbs = new MongoObservable.Collection<Thumb>('thumbs');
 
 export const ThumbsStore = new UploadFS.store.GridFS({
   collection: Thumbs.collection,
-  name: 'thumbs'
+  name: 'thumbs',
+  transformWrite(from, to, fileId, file) {
+    // Resize to 32x32
+    const gm = require('gm');
+
+    gm(from, file.name)
+      .resize(32, 32)
+      .gravity('Center')
+      .extent(32, 32)
+      .quality(75)
+      .stream()
+      .pipe(to);
+  }
 });
 
 export const ImagesStore = new UploadFS.store.GridFS({
