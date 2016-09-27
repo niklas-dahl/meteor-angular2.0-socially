f56b758d597ef6078ed3e5749a194725818ce2a2
dotansimha <dotansimha@gmail.com>
2016-09-27 14:00:46 +0300

    Step 21.5: Add interfaces to Mongo Collections

--- both/collections/images.collection.ts
+++ both/collections/images.collection.ts
@@ -1,8 +1,9 @@
 import { MongoObservable } from 'meteor-rxjs';
 import { Meteor } from 'meteor/meteor';
+import {Thumb, Image} from "../models/image.model";
 
-export const Images = new MongoObservable.Collection('images');
-export const Thumbs = new MongoObservable.Collection('thumbs');
+export const Images = new MongoObservable.Collection<Image>('images');
+export const Thumbs = new MongoObservable.Collection<Thumb>('thumbs');
 
 function loggedIn(userId) {
   return !!userId;
